71367379264b6a55c70b72133afeda8b25029a0d
Gabriel Carvalho <gabrielhenriquerc11@gmail.com>
2024-10-19 12:33:10 -0300

    fix: urls

--- posts/urls.py
+++ posts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import PostListCreateView, PostDetailView,AllPostsListView,PostDeleteView,PostUpdateView
 
 urlpatterns = [
-    path('movies/', PostListCreateView.as_view(), name='post-list-create'),
-    path('movies/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('movies/all/', AllPostsListView.as_view(), name='all-post-list'),  
-    path('movies/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'), 
-    path('movies/update/<int:pk>/', PostUpdateView.as_view(), name='post-update'), 
+    path('tasks/', PostListCreateView.as_view(), name='post-list-create'),
+    path('tasks/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('tasks/all/', AllPostsListView.as_view(), name='all-post-list'),  
+    path('tasks/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'), 
+    path('tasks/update/<int:pk>/', PostUpdateView.as_view(), name='post-update'), 
 ]
