6e7160f60a7ce9b98c35b1aaa5d5a74ca9acc2ac
Gabriel Carvalho <gabrielhenriquerc11@gmail.com>
2024-10-19 21:33:52 -0300

    feat: jwt auth

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Post
 from .serializers import PostSerializer
 
@@ -6,24 +7,29 @@ from rest_framework import status
 from rest_framework.response import Response
 
 class PostListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class AllPostsListView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
 class PostUpdateView(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     partial = True
     
     
 class PostDeleteView(generics.DestroyAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
